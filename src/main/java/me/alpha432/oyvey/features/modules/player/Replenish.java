43a3acc9f55322eae4cf33a388717404a87c25f5
WeFoundDaniel <74555840+WeFoundDaniel@users.noreply.github.com>
2021-09-08 10:34:05 -0700

    Update Replenish.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/Replenish.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Replenish.java
@@ -1,43 +1,50 @@
 package me.alpha432.oyvey.features.modules.player;
 
-import me.alpha432.oyvey.features.modules.Module;
-import me.alpha432.oyvey.features.setting.Setting;
-import me.alpha432.oyvey.util.Timer;
-import net.minecraft.init.Items;
-import net.minecraft.inventory.ClickType;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import me.alpha432.oyvey.features.modules.*;
+import me.alpha432.oyvey.features.setting.*;
+import me.alpha432.oyvey.util.*;
+import java.util.*;
+import net.minecraft.init.*;
+import net.minecraft.item.*;
+import net.minecraft.inventory.*;
+import net.minecraft.entity.player.*;
 
-import java.util.ArrayList;
-
-public class Replenish
-        extends Module {
-    private final Setting<Integer> delay = this.register(new Setting<Integer>("Delay", 0, 0, 10));
-    private final Setting<Integer> gapStack = this.register(new Setting<Integer>("GapStack", 1, 50, 64));
-    private final Setting<Integer> xpStackAt = this.register(new Setting<Integer>("XPStack", 1, 50, 64));
-    private final Timer timer = new Timer();
-    private final ArrayList<Item> Hotbar = new ArrayList();
-
+public class Replenish extends Module
+{
+    private final Setting<Integer> delay;
+    private final Setting<Integer> gapStack;
+    private final Setting<Integer> xpStackAt;
+    private final Setting<Integer> crystalStack;
+    private final Timer timer;
+    private final ArrayList<Item> Hotbar;
+    
     public Replenish() {
-        super("Replenish", "Replenishes your hotbar", Module.Category.PLAYER, false, false, false);
+        super("Replenish", "Replenishes your hotbar", Category.PLAYER, false, false, false);
+        this.delay = (Setting<Integer>)this.register(new Setting("Delay", (T)0, (T)0, (T)10));
+        this.gapStack = (Setting<Integer>)this.register(new Setting("GapStack", (T)1, (T)1, (T)64));
+        this.xpStackAt = (Setting<Integer>)this.register(new Setting("XPStack", (T)1, (T)1, (T)64));
+        this.crystalStack = (Setting<Integer>)this.register(new Setting("CrystalStack", (T)1, (T)1, (T)64));
+        this.timer = new Timer();
+        this.Hotbar = new ArrayList<Item>();
     }
-
+    
     @Override
     public void onEnable() {
-        if (Replenish.fullNullCheck()) {
+        if (fullNullCheck()) {
             return;
         }
         this.Hotbar.clear();
         for (int l_I = 0; l_I < 9; ++l_I) {
-            ItemStack l_Stack = Replenish.mc.player.inventory.getStackInSlot(l_I);
+            final ItemStack l_Stack = Replenish.mc.player.inventory.getStackInSlot(l_I);
             if (!l_Stack.isEmpty() && !this.Hotbar.contains(l_Stack.getItem())) {
                 this.Hotbar.add(l_Stack.getItem());
-                continue;
             }
-            this.Hotbar.add(Items.AIR);
+            else {
+                this.Hotbar.add(Items.AIR);
+            }
         }
     }
-
+    
     @Override
     public void onUpdate() {
         if (Replenish.mc.currentScreen != null) {
@@ -47,14 +54,15 @@ public class Replenish
             return;
         }
         for (int l_I = 0; l_I < 9; ++l_I) {
-            if (!this.RefillSlotIfNeed(l_I)) continue;
-            this.timer.reset();
-            return;
+            if (this.RefillSlotIfNeed(l_I)) {
+                this.timer.reset();
+                return;
+            }
         }
     }
-
-    private boolean RefillSlotIfNeed(int p_Slot) {
-        ItemStack l_Stack = Replenish.mc.player.inventory.getStackInSlot(p_Slot);
+    
+    private boolean RefillSlotIfNeed(final int p_Slot) {
+        final ItemStack l_Stack = Replenish.mc.player.inventory.getStackInSlot(p_Slot);
         if (l_Stack.isEmpty() || l_Stack.getItem() == Items.AIR) {
             return false;
         }
@@ -70,18 +78,21 @@ public class Replenish
         if (l_Stack.getItem().equals(Items.EXPERIENCE_BOTTLE) && l_Stack.getCount() > this.xpStackAt.getValue()) {
             return false;
         }
+        if (l_Stack.getItem().equals(Items.END_CRYSTAL) && l_Stack.getCount() > this.crystalStack.getValue()) {
+            return false;
+        }
         for (int l_I = 9; l_I < 36; ++l_I) {
-            ItemStack l_Item = Replenish.mc.player.inventory.getStackInSlot(l_I);
-            if (l_Item.isEmpty() || !this.CanItemBeMergedWith(l_Stack, l_Item)) continue;
-            Replenish.mc.playerController.windowClick(Replenish.mc.player.inventoryContainer.windowId, l_I, 0, ClickType.QUICK_MOVE, Replenish.mc.player);
-            Replenish.mc.playerController.updateController();
-            return true;
+            final ItemStack l_Item = Replenish.mc.player.inventory.getStackInSlot(l_I);
+            if (!l_Item.isEmpty() && this.CanItemBeMergedWith(l_Stack, l_Item)) {
+                Replenish.mc.playerController.windowClick(Replenish.mc.player.inventoryContainer.windowId, l_I, 0, ClickType.QUICK_MOVE, (EntityPlayer)Replenish.mc.player);
+                Replenish.mc.playerController.updateController();
+                return true;
+            }
         }
         return false;
     }
-
-    private boolean CanItemBeMergedWith(ItemStack p_Source, ItemStack p_Target) {
+    
+    private boolean CanItemBeMergedWith(final ItemStack p_Source, final ItemStack p_Target) {
         return p_Source.getItem() == p_Target.getItem() && p_Source.getDisplayName().equals(p_Target.getDisplayName());
     }
 }
-
